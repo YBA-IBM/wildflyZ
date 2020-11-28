498baf6ab7ced52c1e59d129faff8ad7b9ef7dca
YBA-IBM <47538942+YBA-IBM@users.noreply.github.com>
2020-11-27 23:22:05 -0500

    Update test.sh
    Signed-off-by: YBA-IBM <yoel.ben-avraham@ibm.com>

--- test.sh
+++ test.sh
@@ -9,13 +9,13 @@ export ANSI_RESET="\e[0m"
 echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Tests for wildfly functionality: $ANSI_RESET"
-docker run --rm -name wildfly quay.io/ibmz/wildfly:20.0.1
+docker run --rm -d -name wildfly quay.io/ibmz/wildfly:20.0.1
 docker stop wildfly
 
-docker run --rm -name wildfly -p 8080:8080 -p 9990:9990 quay.io/ibmz/wildfly:20.0.1 /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+docker run --rm -d -name wildfly -p 8080:8080 -p 9990:9990 quay.io/ibmz/wildfly:20.0.1 /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 docker stop wildfly
 
-docker run --rm -name wildfly quay.io/ibmz/wildfly:20.0.1 /opt/jboss/wildfly/bin/domain.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+docker run --rm -d -name wildfly quay.io/ibmz/wildfly:20.0.1 /opt/jboss/wildfly/bin/domain.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 docker stop wildfly
 
 
